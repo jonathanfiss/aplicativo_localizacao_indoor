73a891b05f81a11526f3a6a051794513674a225e
Jonathan Fiss <fissjonathan@hmail.com>
2019-05-11 13:52:25 -0300

    update

--- app/src/main/java/com/example/aplicativo_localizacao_indoor/activity/AdminPontoReferenciaActivity.java
+++ app/src/main/java/com/example/aplicativo_localizacao_indoor/activity/AdminPontoReferenciaActivity.java
@@ -2,8 +2,12 @@ package com.example.aplicativo_localizacao_indoor.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.aplicativo_localizacao_indoor.R;
+import com.example.aplicativo_localizacao_indoor.adapter.PontoReferenciaAdapter;
 
 public class AdminPontoReferenciaActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class AdminPontoReferenciaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_ponto_referencia);
+
+        ListView lvPontoRef = findViewById(R.id.lv_ponto_ref);
+        lvPontoRef.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+//        lvPontoRef.setAdapter(new PontoReferenciaAdapter(AdminListaPontoReferenciaActivity.this, Classe com os dados ));
     }
 }

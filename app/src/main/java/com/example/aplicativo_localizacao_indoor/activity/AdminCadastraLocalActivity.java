a50cb814cea05661127cdd69d39875e32b67cf7d
Jonathan Fiss <fissjonathan@gmail.com>
2019-05-29 12:13:09 -0300

    altereraçõe no layout do cadastro de salas e criada nova activity seleciona ponto

--- app/src/main/java/com/example/aplicativo_localizacao_indoor/activity/AdminCadastraLocalActivity.java
+++ app/src/main/java/com/example/aplicativo_localizacao_indoor/activity/AdminCadastraLocalActivity.java
@@ -23,7 +23,6 @@ public class AdminCadastraLocalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_cadastra_local);
 
-//        final RadioGroup radioGroupAndar = findViewById(R.id.radioGroupAndar);
         Button btCadLocal= findViewById(R.id.btCadLocal);
 
         String[] SETORES = new String[] {"Principal", "Tecnologia em sistemas para Internet", "Design", "Caldelas"};
@@ -35,34 +34,36 @@ public class AdminCadastraLocalActivity extends AppCompatActivity {
         autoCompleteSetor.setAdapter(adapter);
 
         final EditText etDescricaoLocal = findViewById(R.id.etDescricaoLocal);
-
+        RadioGroup radioGroupAndar = findViewById(R.id.radioGroupAndar);
+        radioGroupAndar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioButtonAndar1:{
+                        local.setAndar(1);
+                        Log.d("checked", "1 andar");
+                        break;
+                    }
+                    case R.id.radioButtonAndar2:{
+                        local.setAndar(2);
+                        Log.d("checked", "2 andar");
+                        break;
+                    }
+                    case R.id.radioButtonAndar3:{
+                        local.setAndar(3);
+                        Log.d("checked", "3 andar");
+                        break;
+                    }
+                }
+            }
+        });
         btCadLocal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                radioGroupAndar.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-//                    @Override
-//                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                        switch (checkedId){
-//                            case R.id.radioButtonAndar1:{
-//                                Log.d("checked", "1 andar");
-//                                local.setAndar(1);
-//                                break;
-//                            }
-//                            case R.id.radioButtonAndar2:{
-//                                Log.d("checked", "2 andar");
-//                                local.setAndar(2);
-//                                break;
-//                            }
-//                            case R.id.radioButtonAndar3:{
-//                                Log.d("checked", "3 andar");
-//                                local.setAndar(3);
-//                                break;
-//                            }
-//                        }
-//                    }
-//                });
+
                 local.setCorredor(autoCompleteSetor.getText().toString());
                 local.setDescricao(etDescricaoLocal.getText().toString());
+                local.setSituacao(true);
                 // obtém a referência do database e do nó
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference("dados/local");

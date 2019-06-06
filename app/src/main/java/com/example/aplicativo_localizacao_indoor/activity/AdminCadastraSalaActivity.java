77d96c4b0c50f3c895b0ca614ea83462aa1d760c
Jonathan Fiss <fissjonathan@gmail.com>
2019-06-06 10:02:15 -0300

    verifica se campos foram preenchidos cadastro sala

--- app/src/main/java/com/example/aplicativo_localizacao_indoor/activity/AdminCadastraSalaActivity.java
+++ app/src/main/java/com/example/aplicativo_localizacao_indoor/activity/AdminCadastraSalaActivity.java
@@ -1,14 +1,15 @@
 package com.example.aplicativo_localizacao_indoor.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.aplicativo_localizacao_indoor.R;
 import com.example.aplicativo_localizacao_indoor.model.Local;
@@ -51,27 +52,23 @@ public class AdminCadastraSalaActivity extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                int i = 0;
+                corredor.clear();
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     Local local = ds.getValue(Local.class);
                     local.setKey(ds.getKey());
                     if (AppSetup.local.size()>0){
                         for (Local lc: AppSetup.local){
-                            if (!local.getCorredor().toString().equals(lc.getCorredor().toString())){
+                            if (!local.getCorredor().equals(lc.getCorredor())){
                                 corredor.add(lc.getCorredor());
+                                Log.d("corredorUnidade",lc.getCorredor());
+                                Log.d("corredorUnid",corredor.toString());
                             }
                         }
                     }
-//                    for (i = 0; i < corredor.size(); i++) {
-//                        if (local.getCorredor().equals(corredor.get(i))) {
-//                        } else {
-//                            corredor.add(local.getCorredor());
-//                        }
-//                    }
                     AppSetup.local.add(local);
 
                 }
-                Log.d("banco", "Value is teste: " + corredor.toString());
+                Log.d("corredorTodo",corredor.toString());
             }
 
             @Override
@@ -98,14 +95,20 @@ public class AdminCadastraSalaActivity extends AppCompatActivity {
         btCadastrarSala.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sala.setNome(etNomeSala.getText().toString());
-                sala.setNumero(etNumeroSala.getText().toString());
+                if (!etNomeSala.getText().toString().isEmpty() && !etNumeroSala.getText().toString().isEmpty() && !spLocalSala.toString().isEmpty()){
+                    sala.setNome(etNomeSala.getText().toString());
+                    sala.setNumero(etNumeroSala.getText().toString());
 //                sala.setlocal();
-                sala.setSituacao(true);
-                // obtém a referência do database e do nó
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("dados/sala");
-                myRef.push().setValue(sala);
+                    sala.setSituacao(true);
+                    // obtém a referência do database e do nó
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("dados/sala");
+                    myRef.push().setValue(sala);
+                }else{
+                    Toast.makeText(AdminCadastraSalaActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+
+                }
+
             }
         });
     }
